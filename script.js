561b8f4406063920e1f78fc4e8cdf7d93cc4147b
sivilela <sivilela.s@gmail.com>
2023-07-30 19:51:55 -0300

    Apliquei a responsividade nas seções

---
+++ script.js
@@ -0,0 +1,13 @@
+function abrirAlerta(){
+  // alert("Cadastro realizado com sucesso!")
+  swal("Cupom enviado com sucesso!");
+}
+
+
+// Função Menu. Refazer
+// const btMenu = document.getElementById('bt-nav');
+// function toggleMenu(){
+//   const nav = document.getElementById('nav');
+//   nav.classList.toggle('active');
+// }
+// btMenu.addEventListener('click', toggleMenu);
